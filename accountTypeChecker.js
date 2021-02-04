9011a7754b6212545e789aeefd2dd3d90c749393
Wisdom Adzorgenu <wadzorgenu@gmail.com>
2021-02-04 14:18:31 +0000

    Created account type checker function.

---
+++ accountTypeChecker.js
@@ -0,0 +1,44 @@
+/**
+ * @param {ObjectArray} accountBalanceHistory
+ */
+const accountTypeChecker = (accountBalanceHistory) => {
+
+   //only a valid array of objects is expected
+   let isArrayObject = !!(Array.isArray(accountBalanceHistory) && accountBalanceHistory.every(info=>typeof info === "object"))
+
+   if(!accountBalanceHistory || !isArrayObject)
+      throw new Error("A balance account history is expected")
+
+   //sort account history to start from older months to recent months
+   //sort in descending order of months
+   accountBalanceHistory.sort((a,b) => b.monthNumber - a.monthNumber)
+
+   let previousBalance = null;
+   let previousBalanceDifference = null;
+   let isFixedBalance = true;
+
+   for(let i=0; i<accountBalanceHistory.length; i++){
+      //ensure we have account information
+      let account = accountBalanceHistory[i].account ? accountBalanceHistory[i].account : {}
+
+      //use if balance exists in object else 0
+      let currentBalance = (account && account.balance && account.balance.amount) ? account.balance.amount:0 
+
+      if(previousBalance !== null){
+         let difference = previousBalance - currentBalance
+
+         if(previousBalanceDifference !== null && previousBalanceDifference !== difference){
+            isFixedBalance = false
+            break
+         }
+
+         previousBalanceDifference = difference
+      }
+
+      previousBalance = currentBalance
+   }
+
+   return isFixedBalance ? "B" : "A";
+};
+
+module.exports = accountTypeChecker
